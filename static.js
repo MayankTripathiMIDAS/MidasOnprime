33ab12c5aa5f19bd788de3a54d242c520d270575
mayank <mayank.tripathi@midasconsulting.org>
2024-10-01 23:46:56 +0530

    Changes

--- static.js
+++ static.js
@@ -1,3 +1,3 @@
-export const host = "http://34.230.215.187:9000/";
+export const host = "http://34.230.215.187:9001/";
 // export const host = "http://localhost:9000/";
 // export const host = "http://192.168.1.105:9000";
