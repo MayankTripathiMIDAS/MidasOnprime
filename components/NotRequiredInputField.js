4177d505add6abfd03dbb4a7364473ac66bc4051
MayankTripathiMIDAS <165718550+MayankTripathiMIDAS@users.noreply.github.com>
2025-05-20 04:50:33 +0530

    Update NotRequiredInputField.js

--- components/NotRequiredInputField.js
+++ components/NotRequiredInputField.js
@@ -1,1 +1,47 @@
+import React from "react";
 
+const NotRequiredInputField = ({
+  label,
+  value,
+  type,
+  placeholder,
+  onChange,
+  onBlur,
+  id,
+  name,
+  onClick,
+  disabled,
+  style,
+  maxLength,
+  errors,
+  touched,
+}) => {
+  return (
+    <div className={label === "Request Time Off" ? "col-md-2" : "col-md-3"}>
+      <label className="m-2 text-dark" style={style}>
+        {label}
+      </label>
+      <input
+        type={type}
+        className={`form-control ${touched && errors ? "is-invalid" : ""}`} // Add error class if touched and has error
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        name={name}
+        onClick={onClick}
+        disabled={disabled}
+        style={style}
+        maxLength={maxLength} // Correct attribute
+      />
+      {touched &&
+        errors &&
+        typeof errors === "string" && ( // Ensure errors is a string before rendering
+          <div className="text-danger">{errors}</div>
+        )}
+    </div>
+  );
+};
+
+export default NotRequiredInputField;
