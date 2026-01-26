4bff9907f1cadd553c2f3af357e982ea765081d3
Admin 1 <mayank.tripathi@midasconsulting.org>
2026-01-26 15:25:06 -0600

    Add checklist4 for UAT Testing

--- static.js
+++ static.js
@@ -1,3 +1,4 @@
-export const host = "https://tenantchecklist.theartemis.ai/";
+// export const host = "https://tenantchecklist.theartemis.ai/";
+
 // export const host = "http://localhost:9000/";
-// export const host = "http://192.168.1.33:9002/"
+export const host = "http://192.168.1.42:9002/"
